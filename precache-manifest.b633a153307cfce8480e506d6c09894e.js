56ccd0ab4b43dcde62b6a155377e18f2c9b688d5
Kenneth Nergård <64799756+KennethEBNer@users.noreply.github.com>
2020-08-11 19:16:51 +0200

    Updates

---
+++ precache-manifest.b633a153307cfce8480e506d6c09894e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d3e7ba815db70da7eab242df04c4e17",
+    "url": "/pomodoro-app/index.html"
+  },
+  {
+    "revision": "39fc6e8d9256fa209f5d",
+    "url": "/pomodoro-app/static/css/main.a7b8f64c.chunk.css"
+  },
+  {
+    "revision": "928aad7c610a17d86687",
+    "url": "/pomodoro-app/static/js/2.46abfb5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro-app/static/js/2.46abfb5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39fc6e8d9256fa209f5d",
+    "url": "/pomodoro-app/static/js/main.8eb48340.chunk.js"
+  },
+  {
+    "revision": "e2406da7de486647b6ea",
+    "url": "/pomodoro-app/static/js/runtime-main.57a843a7.js"
+  }
+]);
